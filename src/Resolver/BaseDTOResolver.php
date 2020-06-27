8b8d630ca8258e157e5789f94b9133ed20aaf8ee
VasilevDS <vasilyev0992@gmail.com>
2020-06-27 12:24:19 +0500

    created abstract resolver for test. do later

---
+++ src/Resolver/BaseDTOResolver.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace App\Resolver;
+
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Exception\ValidatorException;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+abstract class BaseDTOResolver implements ArgumentValueResolverInterface
+{
+    const TYPE = BaseDTOResolver::class;
+    protected SerializerInterface $serializer;
+    protected ValidatorInterface $validator;
+
+    public function __construct(SerializerInterface $serializer, ValidatorInterface $validator)
+    {
+        $this->serializer = $serializer;
+        $this->validator = $validator;
+    }
+
+    public function supports(Request $request, ArgumentMetadata $argument)
+    {
+        return self::TYPE === $argument->getType();
+    }
+
+    public function resolve(Request $request, ArgumentMetadata $argument)
+    {
+        $dto = $this->serializer->deserialize($request->getContent(), self::TYPE, 'json');
+
+        $errors = $this->validator->validate($dto);
+        if (count($errors) > 0) {
+            throw new ValidatorException((string)$errors);
+        }
+
+        yield $dto;
+    }
+}
